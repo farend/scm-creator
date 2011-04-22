6c178343435f34bb516277f4d38ca42118c52294
s-andy <s-andy@ea6535c2-5f70-4292-bc30-dee2014a1bba>
2011-04-22 16:49:50 +0000

    Added Git code (thanks Bertrand Baudinet)
    git-svn-id: http://svn.s-andy.com/scm-creator@10 ea6535c2-5f70-4292-bc30-dee2014a1bba

--- lib/repositories_controller_patch.rb
+++ lib/repositories_controller_patch.rb
@@ -26,19 +26,42 @@ module RepositoriesControllerPatch
             if request.post? && @repository
                 if params[:operation].present? && params[:operation] == 'add'
                     if params[:repository]
-                        path = SvnConfig['path'].dup
-                        path.gsub!(/\\/, "/") if Redmine::Platform.mswin?
-                        matches = Regexp.new("^file://#{Regexp.escape(path)}/([^/]+)/?$").match(params[:repository]['url'])
-                        if matches
-                            repath = Redmine::Platform.mswin? ? "#{SvnConfig['path']}\\#{matches[1]}" : "#{SvnConfig['path']}/#{matches[1]}"
-                            if File.directory?(repath)
-                                @repository.errors.add(:url, :already_exists)
+                        if params[:repository_scm] == 'Subversion'
+                            path = SvnConfig['path'].dup
+                            path.gsub!(/\\/, "/") if Redmine::Platform.mswin?
+                            matches = Regexp.new("^file://#{Regexp.escape(path)}/([^/]+)/?$").match(params[:repository]['url'])
+                            if matches
+                                repath = Redmine::Platform.mswin? ? "#{SvnConfig['path']}\\#{matches[1]}" : "#{SvnConfig['path']}/#{matches[1]}"
+                                if File.directory?(repath)
+                                    @repository.errors.add(:url, :already_exists)
+                                else
+                                    RAILS_DEFAULT_LOGGER.info "[EDIT_WITH_ADD] #{SvnConfig['svnadmin']} create #{repath}" # FIXME
+                                    system(SvnConfig['svnadmin'], 'create', repath)
+                                end
                             else
-                                RAILS_DEFAULT_LOGGER.info "[EDIT_WITH_ADD] #{SvnConfig['svnadmin']} create #{repath}" # FIXME
-                                system(SvnConfig['svnadmin'], 'create', repath)
+                                @repository.errors.add(:url, :should_be_of_format_local, :format => "file://#{path}/<#{l(:label_repository_format)}>/")
+                            end
+                        elsif params[:repository_scm] == 'Git'
+                            path = SvnConfig['gitpath'].dup
+                            path.gsub!(/\\/, "/") if Redmine::Platform.mswin?
+                            matches = Regexp.new("^#{Regexp.escape(path)}/([^/]+)/?$").match(params[:repository]['url'])
+                            if matches
+                                repath = Redmine::Platform.mswin? ? "#{SvnConfig['gitpath']}\\#{matches[1]}" : "#{SvnConfig['gitpath']}/#{matches[1]}"
+                                if File.directory?(repath)
+                                    @repository.errors.add(:url, :already_exists)
+                                else
+                                    # TODO: test + separate
+                                    #if Redmine::Platform.mswin?
+                                    #    system("mkdir #{matches}& #{SvnConfig['git']} init --bare #{matches}& XCACLS #{matches} /G #{SvnConfig['owner']}:F /y ")
+                                    #else
+                                    #    system("mkdir #{matches}; #{SvnConfig['git']} init --bare #{matches}; chown -R #{SvnConfig['owner']} #{matches}")
+                                    #end
+                                end
+                            else
+                                @repository.errors.add(:url, :should_be_of_format_local, :format => "file://#{path}/<#{l(:label_repository_format)}>/") # FIXME: file://?
                             end
                         else
-                            @repository.errors.add(:url, :should_be_of_format_local, :format => "file://#{path}/<#{l(:label_repository_format)}>/")
+                            # TODO: @repository.errors.add(:url, :scm_not_supported)
                         end
                     end
                 end
--- lib/repositories_helper_patch.rb
+++ lib/repositories_helper_patch.rb
@@ -8,6 +8,7 @@ module RepositoriesHelperPatch
         base.class_eval do
             unloadable
             alias_method_chain :subversion_field_tags, :add
+            alias_method_chain :git_field_tags, :add
         end
     end
 
@@ -23,13 +24,28 @@ module RepositoriesHelperPatch
                 add = submit_tag(l(:button_create_new_repository), :onclick => "$('repository_operation').value = 'add';")
                 svntags['<br />'] = ' ' + add + '<br />'
                 svntags << hidden_field_tag(:operation, '', :id => 'repository_operation')
-                unless params && params[:repository]
+                unless params && params[:repository] # FIXME: when switching it is not present
                     path = SvnConfig['path'].dup
                     path.gsub!(/\\/, "/") if Redmine::Platform.mswin?
                     svntags << javascript_tag("$('repository_url').value = 'file://#{escape_javascript(path)}/#{@project.identifier}';")
                 end
             end
             return svntags
+        end
+
+        def git_field_tags_with_add(form, repository)
+            gittags = git_field_tags_without_add(form, repository)
+            if !@project.repository && SvnConfig['gitpath'].present?
+                add = submit_tag(l(:button_create_new_repository), :onclick => "$('repository_operation').value = 'add';")
+                gittags['</p>'] = ' ' + add + '</p>'
+                gittags << hidden_field_tag(:operation, '', :id => 'repository_operation')
+                unless params && params[:repository]
+                    path = SvnConfig['gitpath'].dup
+                    path.gsub!(/\\/, "/") if Redmine::Platform.mswin?
+                    gittags << javascript_tag("$('repository_url').value = 'file://#{escape_javascript(path)}/#{@project.identifier}';") # FIXME: file://?
+                end
+            end
+            return gittags
         end
 
     end
