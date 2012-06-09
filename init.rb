3e067e6857d29ee5986392e1ec83dc847069da16
s-andy <s-andy@ea6535c2-5f70-4292-bc30-dee2014a1bba>
2012-06-09 10:50:22 +0000

    Last fix for Redmine 2.0
    git-svn-id: http://svn.s-andy.com/scm-creator@102 ea6535c2-5f70-4292-bc30-dee2014a1bba

--- init.rb
+++ init.rb
@@ -31,5 +31,5 @@ Redmine::Plugin.register :redmine_scm_plugin do
     author_url 'http://www.andriylesyuk.com/'
     description 'Allows creating Subversion, Git, Mercurial and Bazaar repositories using Redmine.'
     url 'http://projects.andriylesyuk.com/projects/redmine-svn'
-    version '0.3.1'
+    version '0.4.0'
 end
