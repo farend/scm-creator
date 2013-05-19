7f1a72dab4471891fc7dd015f3356c9223ebed95
s-andy <s-andy@ea6535c2-5f70-4292-bc30-dee2014a1bba>
2013-05-19 20:38:58 +0000

    Added hidden field for disable repository module field
    git-svn-id: http://svn.s-andy.com/scm-creator@124 ea6535c2-5f70-4292-bc30-dee2014a1bba

--- assets/javascripts/scm_utils_jquery.js
+++ assets/javascripts/scm_utils_jquery.js
@@ -2,7 +2,7 @@ var force_repository_module = function() {
   var r = $('#project_enabled_module_names_repository');
   if (r) {
     r.attr('checked', 'checked')
-    r.attr('readonly','readonly');
+    r.attr('disabled','disabled');
   }
 };
 var toggle_project_scm_visibility = function() {
